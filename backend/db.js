d8527f089c878e86ba069a47952a013392e5dd57
SarvT <91484197+SarvT@users.noreply.github.com>
2023-07-25 23:00:30 +0530

    talks with database precisely

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = "mongodb://localhost:27017";
+const dbURI = "mongodb://localhost:27017/notevote";
 
 const connectDB= ()=> {
     try {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
+router.post(
+  "/",
+  [
+    body("name").isLength({ min: 4 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      return User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then((user) => res.json(user))
+      .catch(err=>{
+        res.json({error: "Already signed up, Try logging in.", message:err.message, status:405})
+      })
+    //   return res.send(`Hello, ${req.query.person}!`);
+    }
 
-router.post('/', (req, res)=>{
-    console.log("Hello from the server");
-    const newUser = User(req.body);
-    newUser.save()
-    res.json( "Welcome to my API");
-})
+    res.send({ errors: result.array() });
+    // console.log("Hello from the server");
+    // const newUser = User(req.body);
+    // newUser.save();
+    // res.json("Welcome to my API");
+  }
+);
 
 module.exports = router;
